06c95d4e3ea27eb942381db0f9d8ced2c143316c
Rusheel <rusheel@caredocs.ai>
2026-02-08 12:45:14 -0800

    Add initial components and models for the application
    - Created AdviceCard and NavBar components
    - Added Advice and User interfaces to models
    - Updated App component to remove console log
    - Added index files for components and models

--- src/fieldtested.client/src/App.tsx
+++ src/fieldtested.client/src/App.tsx
@@ -2,13 +2,11 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import { get } from './proxies/proxy'
 import './App.css';
-import { SimpleObject } from './models/models';
 
 function App() {
   const [message, setMessage] = useState("");
   useEffect(() => {
     get('/').then(r => r.json()).then(r => setMessage(r['message']));
-    console.log(message);
   }, [])
   return (
     <div className="App">
--- src/fieldtested.client/src/models/models.ts
+++ src/fieldtested.client/src/models/models.ts
@@ -1,1 +1,17 @@
 export type SimpleObject = Record<string, any>
+
+export interface Advice {
+    id: number;
+    advice: string;
+    rating: number;
+    implementation: string;
+    intent: string;
+}
+
+export interface User {
+    id: number;
+    username: string;
+    email: string;
+    name?: string;
+    dateOfBirth?: string;
+}
